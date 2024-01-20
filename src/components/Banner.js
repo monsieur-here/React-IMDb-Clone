cc81ff941da7e02c41b57524248a209fe815ec03
Siddharth Dyavanapalli <dyavanapallisiddharth784@gmail.com>
2024-01-20 18:27:28 +0530

    Styling changes in the project

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='flex items-end h-[20vh] md: h-[60vh] bg-center bg-no-repeat' style={{
+    <div className='flex items-end h-[20vh] md: h-[60vh] bg-center bg-no-repeat bg-gray-800' style={{
       backgroundImage: "url(https://assets-in.bmscdn.com/discovery-catalog/events/et00311714-ewdhvajezf-landscape.jpg)"}}>
         {/* Overlay Text */}
         <div className='w-full text-xl bg-gray-900 text-white text-center'>John Wick</div>
